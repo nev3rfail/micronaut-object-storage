9ed180b9d17375306db3c645d7bbcd4d15da0728
nev3rfail <nev3rfail@gmail.com>
2023-05-25 05:22:36 +0300

    cleanup

--- object-storage-minio/src/test/groovy/io/micronaut/objectstorage/aws/AwsS3OperationsCloudSpec.groovy
+++
@@ -1,9 +0,0 @@
-package io.micronaut.objectstorage.aws
-
-import io.micronaut.test.extensions.spock.annotation.MicronautTest
-import io.micronaut.test.support.TestPropertyProvider
-import spock.lang.Requires
-
-@MicronautTest
-@Requires({ env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY && env.AWS_REGION })
-class AwsS3OperationsCloudSpec extends AbstractAwsS3Spec implements TestPropertyProvider {}
